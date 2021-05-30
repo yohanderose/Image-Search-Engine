a5a5c67b3447d5b0e8ec98700a53b6d5d5cd87ee
Yohan de Rose <yohanderose@gmail.com>
2021-05-30 10:07:50 +0000

    feat(frontend): Integrating cognito login UI and functions

--- frontend/upload.php
+++ frontend/upload.php
@@ -1,50 +1,53 @@
 <?php
+
 $target_dir = "uploads/";
 $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
 $uploadOk = 1;
-$imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+$imageFileType = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
 
 // Check if image file is a actual image or fake image
-if(isset($_POST["submit"])) {
-  $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
-  if($check !== false) {
-    // echo "File is an image - " . $check["mime"] . ".";
-    $uploadOk = 1;
-  } else {
-    // echo "File is not an image.";
-    $uploadOk = 0;
-  }
+if (isset($_POST["submit"])) {
+	$check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
+	if ($check !== false) {
+		// echo "File is an image - " . $check["mime"] . ".";
+		$uploadOk = 1;
+	} else {
+		// echo "File is not an image.";
+		$uploadOk = 0;
+	}
 }
 
 // Check if file already exists
 if (file_exists($target_file)) {
-  echo "Sorry, file already exists.";
-  $uploadOk = 0;
+	echo "Sorry, file already exists.";
+	$uploadOk = 0;
 }
 
 // Check file size
 if ($_FILES["fileToUpload"]["size"] > 500000) {
-  echo "Sorry, your file is too large.";
-  $uploadOk = 0;
+	echo "Sorry, your file is too large.";
+	$uploadOk = 0;
 }
 
 // Allow certain file formats
-if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
-&& $imageFileType != "gif" ) {
-  echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
-  $uploadOk = 0;
+if (
+	$imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
+	&& $imageFileType != "gif"
+) {
+	echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+	$uploadOk = 0;
 }
 
 // Check if $uploadOk is set to 0 by an error
 if ($uploadOk == 0) {
-  echo "Sorry, your file was not uploaded.";
-// if everything is ok, try to upload file
+	echo "Sorry, your file was not uploaded.";
+	// if everything is ok, try to upload file
 } else {
-  if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
-    echo "The file ". htmlspecialchars( basename( $_FILES["fileToUpload"]["name"])). " has been uploaded.";
-  } else {
-    echo "Sorry, there was an error uploading your file.";
-  }
+	if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+		echo "The file " . htmlspecialchars(basename($_FILES["fileToUpload"]["name"])) . " has been uploaded.";
+	} else {
+		echo "Sorry, there was an error uploading your file.";
+	}
 }
 
 // Convert moved image to base64
@@ -56,16 +59,16 @@ $base64 = base64_encode($img_data);
 echo "\n" . $base64;
 
 // Send data to S3
-$url = " https://qlpvexadf5.execute-api.us-east-1.amazonaws.com/testing/get-items";
-$data = array('name' => htmlspecialchars( basename( $_FILES["fileToUpload"]["name"])), 'image' => ($base64));
+$url = " https://qlpvexadf5.execute-api.us-east-1.amazonaws.com/testing/upload-to-s3-new";
+$data = array('name' => htmlspecialchars(basename($_FILES["fileToUpload"]["name"])), 'image' => ($base64));
 
 // use key 'http' even if you send the request to https://...
 $options = array(
-    'http' => array(
-        'header'  => "Content-type: application/json",
-        'method'  => 'POST',
-        'content' => http_build_query($data)
-    )
+	'http' => array(
+		'header'  => "Content-type: application/json",
+		'method'  => 'POST',
+		'content' => http_build_query($data)
+	)
 );
 $context  = stream_context_create($options);
 $result = file_get_contents($url, false, $context);
@@ -74,9 +77,8 @@ $result = file_get_contents($url, false, $context);
 unlink($target_file);
 
 $newURL = "http://ec2-3-235-253-98.compute-1.amazonaws.com/";
-if ($result === FALSE) { 
-	echo 'Errors encountered!';
+if ($result === FALSE) {
+	echo 'Errors encountered! Check this new1';
 } else {
-	header('Location: '.$newURL);
+	header('Location: ' . $newURL);
 }
-
